f62eca0dc1a59ce38c466f7d75ee9ccf48716566
Alan Alvarado <aaa240020@utdallas.edu>
2026-04-11 20:40:49 -0500

    Added all of the variables, and their accurate size, that I think I'm going to need for the rest of the code.

--- src/procedural_bookshelf.py
+++ src/procedural_bookshelf.py
@@ -1,2 +1,18 @@
 import maya.cmds as cmds
 
+
+class Bookshelf():
+
+    overall_height = 2  # overall height is the height of the shelf without the legs
+
+    shelf_dividers_height = overall_height / 125  # the width and depth of the dividers are the same as the entire shelf
+
+    shelf_height = overall_height - (overall_height / 62.5)
+    shelf_width = 1
+    shelf_depth = 0.3
+    books_offset = 0  # a slider will be added to adjust
+    shelf_levels = 6
+    books_height = (shelf_height / shelf_levels) - shelf_dividers_height  # a slider will be added to adjust
+    books_depth = shelf_depth - (shelf_depth / 5)  # a slider will be added to adjust
+
+    shelf_leg_height = overall_height / 20
