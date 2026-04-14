74f449b88f637778e90c16bb5307ce39da89e38d
Alan Alvarado <alanalvara@hotmail.com>
2026-04-13 22:43:20 -0500

    All of the variables that are directly affected by a customizable variable didn't update, so I fixed it by getting rid of all extra variables at the cost of readability.

--- src/procedural_bookshelf.py
+++ src/procedural_bookshelf.py
@@ -69,33 +69,33 @@ class Bookshelf():
 
     overall_height = 2  # the height of the shelf without the legs
 
-    shelf_height = overall_height - (overall_height / 62.5)
+    #shelf_height = overall_height - (overall_height / 62.5)
     shelf_width = 1  # an option
     shelf_depth = 0.3  # an option
     shelf_levels = 6  # an option
-
-    shelf_dividers_height = overall_height / 125
-    shelf_dividers_width = shelf_width - (shelf_dividers_height * 2)
-
+    
+    #shelf_dividers_height = overall_height / 125
+    #shelf_dividers_width = shelf_width - ((overall_height / 125) * 2)
+    
     books_offset = 0  # a slider will be added to adjust
-    books_height = (shelf_height / shelf_levels) - shelf_dividers_height  # a slider will be added to adjust
-    books_depth = shelf_depth - (shelf_depth / 10)  # a slider will be added to adjust
+    #books_height = ((overall_height - (overall_height / 62.5)) / shelf_levels) - (overall_height / 125)  # a slider will be added to adjust
+    #books_depth = shelf_depth - (shelf_depth / 10)  # a slider will be added to adjust
 
-    shelf_leg_height = overall_height * (1/25)
+    #shelf_leg_height = overall_height * (1/25)
 
     def generate_dividers(self):
         dividers = []
         for level in range(1, self.shelf_levels):
-            divider = cmds.polyCube(height=self.shelf_dividers_height,
-                                    width=self.shelf_dividers_width,
+            divider = cmds.polyCube(height=(self.overall_height / 125),
+                                    width=(self.shelf_width - ((self.overall_height / 125) * 2)),
                                     depth=self.shelf_depth,
                                     name="divider_plank_1")[0]
             cmds.xform(divider,
                        translation=[0,
-                                    ((self.shelf_height/2) +
-                                     (self.shelf_dividers_height/2)) -
-                                    (((self.shelf_height +
-                                       self.shelf_dividers_height) /
+                                    (((self.overall_height - (self.overall_height / 62.5))/2) +
+                                     ((self.overall_height / 125)/2)) -
+                                    ((((self.overall_height - (self.overall_height / 62.5)) +
+                                       (self.overall_height / 125)) /
                                      self.shelf_levels) * level),
                                     0])
             self._freeze_transforms(divider)
@@ -108,17 +108,17 @@ class Bookshelf():
         legs = []
         for x_axis_duplicates in [-1, 1]:
             for z_axis_dupllicates in [-1, 1]:
-                leg = cmds.polyCylinder(height=self.shelf_leg_height,
-                                        radius=self.shelf_leg_height/2.5,
+                leg = cmds.polyCylinder(height=(self.overall_height * (1/25)),
+                                        radius=(self.overall_height * (1/25))/2.5,
                                         name="shelf_leg_1")[0]
                 cmds.xform(leg,
                            translation=[(((self.shelf_width/2) -
-                                        (self.shelf_dividers_height * 4)) *
+                                        ((self.overall_height / 125) * 4)) *
                                         x_axis_duplicates),
                                         (-self.overall_height/2) -
-                                        self.shelf_leg_height/2,
+                                        (self.overall_height * (1/25))/2,
                                         (((self.shelf_depth/2) -
-                                         (self.shelf_dividers_height * 4)) *
+                                         ((self.overall_height / 125) * 4)) *
                                         z_axis_dupllicates),])
                 self._freeze_transforms(leg)
                 legs.append(leg)
@@ -129,25 +129,25 @@ class Bookshelf():
     def generate_frame(self):
         frame = []
         for duplicates in range(-1, 2, 2):
-            vertical_plank = cmds.polyCube(height=self.shelf_height,
-                                           width=self.shelf_dividers_height,
+            vertical_plank = cmds.polyCube(height=(self.overall_height - (self.overall_height / 62.5)),
+                                           width=(self.overall_height / 125),
                                            depth=self.shelf_depth,
                                            name="vertical_plank_1")[0]
             cmds.xform(vertical_plank,
                        translation=[((self.shelf_width/2) -
-                                    self.shelf_dividers_height/2) *
+                                    (self.overall_height / 125)/2) *
                                     duplicates,
                                     0, 0])
             self._freeze_transforms(vertical_plank)
             frame.append(vertical_plank)
-            horizontal_plank = cmds.polyCube(height=self.shelf_dividers_height,
+            horizontal_plank = cmds.polyCube(height=(self.overall_height / 125),
                                              width=self.shelf_width,
                                              depth=self.shelf_depth,
                                              name="horizontal_plank_1")[0]
             cmds.xform(horizontal_plank,
                        translation=[0,
-                                    ((self.shelf_height/2) +
-                                     self.shelf_dividers_height/2) *
+                                    (((self.overall_height - (self.overall_height / 62.5))/2) +
+                                     (self.overall_height / 125)/2) *
                                     duplicates,
                                     0])
             self._freeze_transforms(horizontal_plank)
@@ -155,13 +155,13 @@ class Bookshelf():
 
         back_plank = cmds.polyCube(height=self.overall_height,
                                    width=self.shelf_width,
-                                   depth=self.shelf_dividers_height,
+                                   depth=(self.overall_height / 125),
                                    name="back_plank")[0]
         cmds.xform(back_plank,
                    translation=[0,
                                 0,
                                 -(self.shelf_depth/2) -
-                                (self.shelf_dividers_height/2)])
+                                ((self.overall_height / 125)/2)])
         self._freeze_transforms(back_plank)
         frame.append(back_plank)
 
@@ -173,17 +173,17 @@ class Bookshelf():
     def generate_books(self):
         pile_of_books = []
 
-        book_x_axis = -(self.shelf_dividers_width/2) + self.books_offset
+        book_x_axis = -((self.shelf_width - ((self.overall_height / 125) * 2))/2) + self.books_offset
 
-        while book_x_axis < (self.shelf_dividers_width/2):
+        while book_x_axis < ((self.shelf_width - ((self.overall_height / 125) * 2))/2):
             random_width = random.uniform(self.overall_height/100,
                                           self.overall_height/40)
-            random_height = random.uniform(self.books_height/1.83529,
-                                           self.books_height)
-            random_depth = random.uniform(self.books_depth/2.16,
-                                          self.books_depth)
+            random_height = random.uniform((((self.overall_height - (self.overall_height / 62.5)) / self.shelf_levels) - (self.overall_height / 125))/1.83529,
+                                           (((self.overall_height - (self.overall_height / 62.5)) / self.shelf_levels) - (self.overall_height / 125)))
+            random_depth = random.uniform((self.shelf_depth - (self.shelf_depth / 10))/2.16,
+                                          (self.shelf_depth - (self.shelf_depth / 10)))
 
-            if book_x_axis + random_width > (self.shelf_dividers_width/2):
+            if book_x_axis + random_width > ((self.shelf_width - ((self.overall_height / 125) * 2))/2):
                 break
 
             book = cmds.polyCube(height=random_height,
@@ -196,7 +196,7 @@ class Bookshelf():
             cmds.xform(book,
                        translation=[book_x_axis + random_width,
                                     random_height/2,
-                                    (-self.books_depth/2) +
+                                    (-(self.shelf_depth - (self.shelf_depth / 10))/2) +
                                     (random_depth/2),])
             book_x_axis += random_width
             self._freeze_transforms(book)
@@ -213,10 +213,10 @@ class Bookshelf():
             sub_grp_name = self.generate_books()
             cmds.xform(sub_grp_name,
                        translation=[0,
-                                    ((self.shelf_height/2) +
-                                     (self.shelf_dividers_height/2)) -
-                                    (((self.shelf_height +
-                                       self.shelf_dividers_height) /
+                                    (((self.overall_height - (self.overall_height / 62.5))/2) +
+                                     ((self.overall_height / 125)/2)) -
+                                    ((((self.overall_height - (self.overall_height / 62.5)) +
+                                       (self.overall_height / 125)) /
                                      self.shelf_levels) * book_stack),
                                     0])
             self._freeze_transforms(sub_grp_name)
@@ -236,12 +236,12 @@ class Bookshelf():
         cmds.xform(grp_name,
                    translation=[0,
                                 (self.overall_height/2) +
-                                self.shelf_leg_height,
+                                (self.overall_height * (1/25)),
                                 0])
         cmds.xform(grp_name,
                    pivots=[0,
                            (-self.overall_height/2) -
-                           self.shelf_leg_height,
+                           (self.overall_height * (1/25)),
                            0])
 
         return grp_name
